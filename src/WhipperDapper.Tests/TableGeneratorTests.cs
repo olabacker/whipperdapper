035a9f0ea4fbd883a219d520a508064b965476be
ola bäcker <olabacker@gmail.com>
2023-02-22 23:51:04 +0100

    test

---
+++ src/WhipperDapper.Tests/TableGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using Dapper.Contrib.Extensions;
+using FluentAssertions;
+
+namespace WhipperDapper.Tests;
+
+public class TableGeneratorTests
+{
+    private readonly TableGenerator _tableGenerator = new();
+
+    [Fact]
+    public void Test1()
+    {
+        var createString = _tableGenerator.GenerateCreateQuery<Whipper>();
+
+        createString.Should().Be("CREATE TABLE IF NOT EXISTS whipper (Id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (Id))");
+    }
+    
+    
+    [Table("whipper")]
+    private class Whipper : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
